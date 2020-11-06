68620afcb4f48a978c9011df4b68ab470f86abe8
Yannick <yannick.schuitemaker@stud.hslu.ch>
2020-11-06 10:35:54 +0100

    Initial
    Initial

---
+++ IOTCode/Lamps/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lamps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
